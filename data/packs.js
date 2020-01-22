117b0efa6202bb8123cf81a613e80cfe4967b3b1
Kyle Devine <devinenoise@gmail.com>
2020-01-21 20:57:29 -0800

    created product data

---
+++ data/packs.js
@@ -0,0 +1,81 @@
+const eightOhEightGold = {
+    id: 'eightOhEightGold',
+    name: '808 Gold',
+    image: '808-gold.png',
+    description: 'Heavy 808 drums processed for maximum punch',
+    category: 'drum-kit',
+    price: 19.99,
+
+};
+
+const hyperspaceLoops = {
+    id: 'hyperspaceLoops',
+    name: 'Hyperspace Loops',
+    image: 'hyperspace-loops.png',
+    description: 'Spacey analog synth sounds processed with vintage tape machines',
+    category: 'loops',
+    price: 19.99,
+
+};
+
+const infinityPack = {
+    id: 'infinityPack',
+    name: 'Infinity Pack',
+    image: 'infinity-pack.png',
+    description: '2020 is the year of infinity.  Take your production to the next level with these futuristic loops.',
+    category: 'loops',
+    price: 19.99,
+
+};
+
+const lofiSoul = {
+    id: 'lofiSoul',
+    name: 'Lofi Soul',
+    image: 'lofi-soul.png',
+    description: 'Detuned and lofi grooves from vintage analog synths.',
+    category: 'loops',
+    price: 19.99,
+
+};
+
+const lushVintage = {
+    id: 'lushVintage',
+    name: 'Lush Vintage',
+    image: 'lush-vintage.png',
+    description: 'usty grooves and vintage electric pianos',
+    category: 'loops',
+    price: 19.99,
+
+};
+
+const machineAge = {
+    id: 'machineAge',
+    name: 'Machine Age',
+    image: 'machine-age.png',
+    description: 'Scrapes, bangs, and crashes from an abandoned factory',
+    category: 'drums',
+    price: 19.99,
+
+};
+
+const playgroundDrums = {
+    id: 'playgroundDrums',
+    name: 'Playground Drums',
+    image: 'playground-drums.png',
+    description: 'Sounds captured from playground equipment',
+    category: 'drums',
+    price: 19.99,
+
+};
+
+const packs = [
+    eightOhEightGold,
+    hyperspaceLoops,
+    infinityPack,
+    lofiSoul,
+    lushVintage,
+    machineAge,
+    playgroundDrums
+];
+
+export default packs;
